5bc97f776ceebb7c711e32b410fc16d7cd8fb4f7
Vinay Reddy <vinayreddyav@outlook.com>
2018-06-06 14:03:53 +0530

    delete code

--- PostProcessingRules.Infrastructure/MonogdbCollectionName.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PostProcessingRules.Persistence
-{
-    public static class MonogdbCollectionName
-    {
-        public const string AlternativeTitles = "AlternativeTitles";
-        public const string AlternativeValues = "AlternativeValues";
-    }
-}
